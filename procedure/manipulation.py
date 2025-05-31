b4c81a81f4ce313f793158c144474319fd97fccc
jisnoo123 <jisnoodevpal@gmail.com>
2025-05-31 23:59:33 +0530

    Add vectorized implementation of the code for faster execution

--- procedure/manipulation.py
+++ procedure/manipulation.py
@@ -1,59 +1,29 @@
 import cv2
+import numpy as np
 from tqdm import tqdm
 
-
-def resize_img(img, ht, wd):
-    '''Resizes an image into (ht*wd)'''
-    resized_img = cv2.resize(img, (ht,wd))
-    return resized_img
+def mean(img):
+    '''Vectorized version - Finds the mean of the pixels of the image of 3 channels'''
+    return np.mean(img)
 
 def blur(img, kernel_size):
     '''Applies Gaussian Blur to an image img of kernel_size'''
     blurred_img = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
     return blurred_img
 
-def mean(img):
-    '''Finds the mean of the pixels of the image of 3 channels'''
-    sum = 0
-    m = img.shape[0]
-    n = img.shape[1]
-    
-    for i in range(m):
-        for j in range(n):
-            sum += int(img[i][j][0]) + int(img[i][j][1]) + int(img[i][j][2])
-    
-    return sum/(m*n*3) # The mean
-
 def linear_search(mean_big_patch, avg_px):
-    '''
-    Arguments:
-        mean_big_patch: Mean of the bigger image's patch
-        avg_px: A list containing the values of the avergae pixels of the dataset
-
-    Returns the index of the min img
-    '''
-    min = abs(mean_big_patch - avg_px[0]) # Min distance
-    min_ind = 0                           # Min index
-
-    # Finding the min img from the dataset
-    for i in range(1,len(avg_px)):
-        if min > abs(mean_big_patch - avg_px[i]):
-            min = abs(mean_big_patch - avg_px[i])
-            min_ind = i
-
-    return min_ind          # Return the index of the min image
-
+    '''Returns the index of the image in the dataset having nearest pixel intensity
+       to the patch'''
+    
+    distances = np.abs(np.array(avg_px) - mean_big_patch)
+    return np.argmin(distances)
 
 def core(big_img, m, n, fds, rb_av):
     '''
-    This function does the whole job, hence the name : core. It returns the new collaged image.
-
-    It extracts patches from the big image having grids mxn, finds the 
-    min img from the dataset and replaces the patches.
-
-    Finally it returns the final collaged image    
+    Vectorized version of core function with minimal changes.
     '''
     big_img_cpy = big_img.copy()
+    # rb_av = np.array(rb_av)  # Convert to numpy array for vectorized operations
 
     y_start = 0
     for i in tqdm(range(1,m+1)):
@@ -66,16 +36,16 @@ def core(big_img, m, n, fds, rb_av):
             blurred_img = blur(cropped_img, 3)
             mean_big_img = mean(blurred_img)
 
-            dataset_img_ind = linear_search(mean_big_img, rb_av)   #Dataset index of img to be replaced
+            dataset_img_ind = linear_search(mean_big_img, rb_av)
 
-            dataset_img = fds[dataset_img_ind]   #The actual dataset img to be placed in place of patch
-            #Resize the dataset img
+            dataset_img = fds[dataset_img_ind]
+
+            # Resize the image to fit in the patch
             dataset_img = cv2.resize(dataset_img, (int(big_img_cpy.shape[1]/n),int(big_img_cpy.shape[0]/m)))
             
-            #Replace the patch with the dataset img retrieved
+            # Replace the dataset image in the patch
             big_img_cpy[y_start:y_start+int(big_img_cpy.shape[0]/m), x_start:x_start+int(big_img_cpy.shape[1]/n)] = dataset_img
             x_start = x_end
         y_start = y_end
 
     return big_img_cpy
-
