cfe426f35e77d83a53170f1b711aded2643976d0
jisnoo123 <jisnoodevpal@gmail.com>
2025-06-06 16:35:19 +0530

    Dataset fix by google drive

--- dataset/create_datasets.py
+++ dataset/create_datasets.py
@@ -20,66 +20,66 @@ sys.path.insert(1, '../procedure')
 from manipulation import *
 
 
-# bashCommand = "chmod u+rx download_dataset.bash"
-# process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-# output, error = process.communicate()
+bashCommand = "chmod u+rx download_dataset.bash"
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
 
-# subprocess.call("./download_dataset.bash")
+subprocess.call("./download_dataset.bash")
 
-# print('Downloading anime dataset and imagenet is done')
-
-
-# print('Extracting the zip files')
-
-# with zipfile.ZipFile('./anime.zip', 'r') as zip_ref:
-#     zip_ref.extractall('./')
+print('Downloading anime dataset and imagenet is done')
 
 
-# with zipfile.ZipFile('./imagenet10.zip', 'r') as zip_ref:
-#     zip_ref.extractall('./')
+print('Extracting the zip files')
 
-# print('Extracting done')
+with zipfile.ZipFile('./anime.zip', 'r') as zip_ref:
+    zip_ref.extractall('./')
+
+
+with zipfile.ZipFile('./imagenet10.zip', 'r') as zip_ref:
+    zip_ref.extractall('./')
+
+print('Extracting done')
 
 
 #------------ Downloading external datasets done -------------- #
 
 
-# # Creating final dataset for CIFAR 10
+# Creating final dataset for CIFAR 10
 
-# print('Processing CIFAR 10 to make the final dataset')
+print('Processing CIFAR 10 to make the final dataset')
 
-# train_dataset = torchvision.datasets.CIFAR10(root='CIFAR10', train=True, download=True)
-# test_dataset = torchvision.datasets.CIFAR10(root='CIIFAR10', train=False, download=True)
-# train_images = [np.array(train_dataset[i][0]) for i in range(len(train_dataset))]
-# test_images = [np.array(test_dataset[i][0]) for i in range(len(test_dataset))]
-# full_dataset = train_images + test_images
+train_dataset = torchvision.datasets.CIFAR10(root='CIFAR10', train=True, download=True)
+test_dataset = torchvision.datasets.CIFAR10(root='CIIFAR10', train=False, download=True)
+train_images = [np.array(train_dataset[i][0]) for i in range(len(train_dataset))]
+test_images = [np.array(test_dataset[i][0]) for i in range(len(test_dataset))]
+full_dataset = train_images + test_images
 
-# with open('cifar10', 'wb') as f:
-#     pickle.dump(full_dataset, f)
-
-
-# '''Resize and blur and avg cifar 10 and pickle it'''
-
-# dataset = list()
-
-# with open('cifar10', 'rb') as f:
-#     dataset = pickle.load(f)
-
-# rb_av = list()
+with open('cifar10', 'wb') as f:
+    pickle.dump(full_dataset, f)
 
 
-# for i in tqdm(range(len(dataset))):
-#     img = dataset[i]
-#     resized_img = resize(img, 10, 10)
-#     blurred_img = blur(resized_img, 3)
-#     mean_pix = mean(blurred_img)
-#     rb_av.append(mean_pix)
+'''Resize and blur and avg cifar 10 and pickle it'''
+
+dataset = list()
+
+with open('cifar10', 'rb') as f:
+    dataset = pickle.load(f)
+
+rb_av = list()
 
 
-# with open('cifar10_rb_av', 'wb') as f:
-#     pickle.dump(rb_av, f)
+for i in tqdm(range(len(dataset))):
+    img = dataset[i]
+    resized_img = resize(img, 10, 10)
+    blurred_img = blur(resized_img, 3)
+    mean_pix = mean(blurred_img)
+    rb_av.append(mean_pix)
 
-# print('Processed CIFAR10 dataset is created')
+
+with open('cifar10_rb_av', 'wb') as f:
+    pickle.dump(rb_av, f)
+
+print('Processed CIFAR10 dataset is created')
 
 
 # Creating final dataset for SVHN
---
+++ dataset/google_drive.py
@@ -0,0 +1,11 @@
+import gdown
+import subprocess
+
+# # a folder
+# url = "https://drive.google.com/drive/folders/1SQRQaJaj_AbQAbjwm8q4efPht-lr3h9J?usp=sharing"
+# gdown.download_folder(url)
+
+
+bashCommand = "mv dataset/actual dataset/rb_av ./"
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
