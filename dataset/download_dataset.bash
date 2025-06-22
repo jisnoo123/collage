54b0fe8752517110d6e5dd1e050882a8f2fce9d2
jisnoo123 <jisnoodevpal@gmail.com>
2025-06-22 23:02:03 +0530

    Dataset fix

--- dataset/download_dataset.bash
+++ dataset/download_dataset.bash
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+sudo apt install unzip
+
 curl -L -o anime.zip\
   https://www.kaggle.com/api/v1/datasets/download/diraizel/anime-images-dataset
 
 #!/bin/bash
 curl -L -o imagenet10.zip\
   https://www.kaggle.com/api/v1/datasets/download/liusha249/imagenet10
+
+unzip anime.zip
+
+unzip imagenet10.zip
